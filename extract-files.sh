28f8d872420bdd35599e1bb313b9cbb935b84b5b
lahaina <nattolecats@gmail.com>
2024-07-16 19:39:09 +0900

    cupida: Solve more vndk issues for camerahalserver * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/camerahalserver": cannot locate symbol "_ZN7android8String16C1EOS0_" referenced by "/vendor/lib64/libbinder.so"... * F linker  : CANNOT LINK EXECUTABLE "/vendor/bin/hw/camerahalserver": cannot locate symbol "_ZN7android8String16aSEOS0_" referenced by "/vendor/lib64/libhidlbase.so"...

--- extract-files.sh
+++ extract-files.sh
@@ -41,6 +41,8 @@ function blob_fixup {
             ;;
         vendor/bin/hw/camerahalserver)
             "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libbinder.so" "libbinder-v32.so" "${2}"
+            "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
         vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
             grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
