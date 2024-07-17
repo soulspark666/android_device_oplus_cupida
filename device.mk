e78334ddc1c641b1f046278ca9523759ebc8ad12
krasCGQ <kras@raphielgang.org>
2024-07-17 11:57:33 +0530

    cupida: Inherit several Android Go configurations Beneficial for everyone, even on non-Go devices.

--- device.mk
+++ device.mk
@@ -19,6 +19,10 @@ DEVICE_PATH := device/oplus/cupida
 # Inherit from ossi device
 $(call inherit-product, device/oplus/mt6893-common/ossi.mk)
 
+# Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayCupida \
