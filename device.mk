06894a8078c586132329a075906d03fad2d504cb
ManshuTyagi <himanshut567@gmail.com>
2024-12-12 16:50:08 +0000

    cupida: Nuke libldacBT_bco

--- device.mk
+++ device.mk
@@ -23,9 +23,6 @@ $(call inherit-product, device/oplus/mt6893-common/common.mk)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
 PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
 
-PRODUCT_PACKAGES += \
-    libldacBT_bco
-
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayCupida \
