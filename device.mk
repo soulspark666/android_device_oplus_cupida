65ea2e35c1dc72520e288cf55d38c6bed4b9e81a
Zeyy <zaidanagusta319@gmail.com>
2025-05-20 06:49:56 +0000

    cupida: Set correct path for image-profile

--- device.mk
+++ device.mk
@@ -21,7 +21,7 @@ $(call inherit-product, device/oplus/mt6893-common/common.mk)
 
 # Inherit several Android Go Configurations (Beneficial for everyone, even on non-Go devices)
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/boot/boot-image-profile.txt
 
 # Overlays
 PRODUCT_PACKAGES += \
